2bbb35f96d00a15d457e8db227cf525e326dfccf
samedyhunx <samedy.hunx@gmail.com>
2026-01-05 15:35:04 +0700

    navigation bar

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="font-Montserrat container">
-    <NuxtPage />
+  <div class="font-Montserrat">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>
---
+++ app/layouts/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Login layout</h1>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
